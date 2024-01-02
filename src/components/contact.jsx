80345d614469082fd4af07ea5be8dc044fdb2d67
Sachin Parihar <130788900+Sheenu-exe@users.noreply.github.com>
2024-01-02 20:04:10 +0530

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -14,27 +14,27 @@ export default function Contact() {
       Reach Out for Opportunities, Collaboration, or Just to Say Hello!
       </p>
       <div className="mt-8 flex flex-wrap justify-center gap-4">
-        <div className="stacked flex flex-column">
+        <div className="stacked  bg-gray-900 ">
             <h3 style={{color:"#4f46e5"}} className="text-black font-semibold text-2xl my-2">Contact Me</h3>
-            <h3 className="flex flex-row font-semibold my-2 text-black">
+            <h3 className="flex flex-row font-semibold my-2 text-white">
               <Phone stroke="#4f46e5" />Call: +91 8668369314 
             </h3>
-            <h3 className="flex flex-row font-semibold text-base text-black my-2">
+            <h3 className="flex flex-row font-semibold text-base text-white my-2">
               <Mail stroke="#4f46e5" />Mail:Pariharsachin5002@gmail.com
             </h3>
         </div>
-        <div className="stacked">
-        <h3 style={{color:"#4f46e5"}} className="text-black font-semibold text-2xl my-2">Address:</h3>
-        <h3 className=" w-full flex flex-row justify-center font-semibold text-base text-black my-4">
-              <MapPin stroke="#4f46e5" />Ram nagar , Rahatani <br />Pune : 411017
+        <div className="stacked  bg-gray-900">
+        <h3 style={{color:"#4f46e5"}} className="text-white font-semibold text-2xl my-2">Address:</h3>
+        <h3 className=" w-full flex flex-row justify-center font-semibold text-lg text-gray-100 my-3">
+              <MapPin stroke="#4f46e5" /> Ram nagar , Rahatani <br />Pune : 411017
             </h3>
         </div>
-        <div className="stacked">
-        <h3 style={{color:"#4f46e5"}} className="text-black font-semibold text-2xl my-2">Social:</h3>
-        <div className="grid grid-cols-3 w-full justify-center">
-         <a href=""><Github height={60} width={60} stroke="black"/></a>
-         <a href=""><Instagram height={60} width={60} stroke="black"/></a>
-         <a href=""><Linkedin height={60} width={60} stroke="black"/></a>
+        <div className="stacked  bg-gray-900">
+        <h3 style={{color:"#4f46e5"}} className="text-black  font-semibold text-2xl my-2">Social:</h3>
+        <div className="flex flex-row w-full justify-center">
+         <a className="mx-5" href="https://github.com/Sheenu-exe"><Github height={60} width={60} stroke="#4f46e5"/></a>
+         <a className="mx-5" href="https://www.instagram.com/sachinn.code/"><Instagram height={60} width={60} stroke="#4f46e5"/></a>
+         <a className="mx-5" href="https://www.linkedin.com/in/sachin-parihar-008180264/"><Linkedin height={60} width={60} stroke="#4f46e5"/></a>
          <a href=""></a>
         </div>
         </div>
