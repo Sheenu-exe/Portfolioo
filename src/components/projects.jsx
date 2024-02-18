2c9ae08426c9cb3b0ff79ee2a0d9bd9758d253c9
Sachin Parihar <130788900+Sheenu-exe@users.noreply.github.com>
2024-02-18 15:12:50 +0530

    Update projects.jsx

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -14,6 +14,52 @@ export default function Projects() {
         <h1 className="text-white font-bold w-full flex justify-center align-center text-6xl">
           PROJECTS
         </h1>
+             <div data-aos="fade-up" className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0 md:rounded-3xl">
+            <svg
+              viewBox="0 0 1024 1024"
+              className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
+              aria-hidden="true"
+            >
+              <circle cx={512} cy={512} r={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
+              <defs>
+                <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                  <stop stopColor="#7775D6" />
+                  <stop offset={1} stopColor="#E935C1"/>
+                </radialGradient>
+              </defs>
+            </svg>
+            <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+              <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                Synapse
+                <br />
+              </h2>
+              <p className="mt-6 text-lg leading-8 text-gray-300">
+             A Flash Card saver app using React, Tailwind and Firebase.
+              </p>
+              <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+                <a
+                  href="https://synapse3.vercel.app/"
+                  target="_blank"
+                  className="rounded-md px-3.5 py-2.5 text-sm font-semibold text-gray-900  " rel="noreferrer"
+                  style={{background:"#4f46e5"}}
+                >
+                  Link
+                </a>
+                <a href="https://github.com/Sheenu-exe/Synapse3" className="bg-white font-semibold text-gray-800 py-2 px-4 rounded-md transition duration-300 ease-in-out hover:shadow-md hover:bg-gray-100">
+                  Source Code <span aria-hidden="true">→</span>
+                </a>
+              </div>
+            </div>
+            <div className="relative mt-16 h-80 lg:mt-8">
+              <img
+                className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
+                src="https://i.ibb.co/Gsmtfgv/Screenshot-2024-02-12-235524.png"
+                alt="App screenshot"
+                width={1824}
+                height={1080}
+              />
+            </div>
+          </div>
             <div data-aos="fade-up" className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0 md:rounded-3xl">
             <svg
               viewBox="0 0 1024 1024"
